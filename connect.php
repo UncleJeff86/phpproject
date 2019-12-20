5b6cb22031a56f293b7a27e4c86f42bff66bc97b
UncleJeff86 <43415081+UncleJeff86@users.noreply.github.com>
2019-12-20 16:58:30 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-#working on localhost at HOME
+#credentials on localhost on HOME SERVER
 
   $servername = "localhost";
   $username = "root";
@@ -12,19 +12,21 @@
   or die('Error connecting.');
           #displays conncetion error
 
-#working on localhost ONLINE
-/*
+#working on localhost on AZURE SERVER
+/* remove this comment line to enable connecting on azure server
+
 $servername = "127.0.0.1:51914/data/mysql/localdb";
 $username = "azure";
 $password = "6#vWHD_$";
 $database = "localdb";
-*/
+
+*/ # remove this comment line to enable connecting on azure server
+
 // Creating connection
 $dbc = mysqli_connect($servername, $username, $password, $database)
 // Checking connection
 or die('Error connecting.');
         #displays connection error
 
-#change
 
 ?>
